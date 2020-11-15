fe392d2b18ce271b33a182b0c0c11515ae5004d7
liao <liaorio@gmail.com>
2020-11-14 23:36:58 -0500

    master clean

--- demo.js
+++ demo.js
@@ -1,15 +1,3 @@
-function test() {
-    console.log('test');
-}
-
-function test2() {
-    console.log('some change');
-}
-
-function test3() {
-    console.log('cherry')
-}
-
-function add() {
-    console.log('add some new features');
+function master() {
+  console.log("test");
 }
